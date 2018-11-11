f4ba5b985bc6687e1156f1fdb8825d4aef4a3c6a
杨秀元 <yangxiuyuan@yangxiuyuandeMacBook-Air.local>
2018-11-11 16:05:26 +0800

    Vue.use 方法...

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -18,3 +18,23 @@ var b = {
 }
 
 export default {a,b,install};
+
+// function initUse (Vue) {
+//     Vue.use = function (plugin) {
+//       var installedPlugins = (this._installedPlugins || (this._installedPlugins = []));
+//       if (installedPlugins.indexOf(plugin) > -1) {
+//         return this
+//       }
+  
+//       // additional parameters
+//       var args = toArray(arguments, 1);
+//       args.unshift(this);
+//       if (typeof plugin.install === 'function') {
+//         plugin.install.apply(plugin, args);
+//       } else if (typeof plugin === 'function') {
+//         plugin.apply(null, args);
+//       }
+//       installedPlugins.push(plugin);
+//       return this
+//     };
+//   }
